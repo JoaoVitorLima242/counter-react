e327fca1aff77d15035e879e9ec8386c2a4c2b58
JoaoVitorLima242 <jlima0500@gmail.com>
2022-07-13 11:50:40 -0300

    increment function

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,13 @@
 import { createStore } from 'redux';
 
 const ReducerFunction = (state={ counter: 0 }, action) => {
+    // Synchronous function
+    // We should not mutate the original state
+
+    if( action.type === 'INC') {
+        return {counter: state.counter + 1}
+    }
+
     return state
 }
 
